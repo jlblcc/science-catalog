54f94c2a96dcbaad301a497a0d47ba178086fdd1
Paul Adams <adamspe@gmail.com>
2018-05-14 15:54:36 -0500

    make sctype-select just a set of radio buttons.

--- app_src/src/app/search/sctype-select.component.ts
+++ app_src/src/app/search/sctype-select.component.ts
@@ -8,79 +8,29 @@ import { SearchService, SearchControl } from './search.service';
 @Component({
     selector: 'sctype-select',
     template: `
-    <mat-button-toggle-group multiple>
-        <mat-button-toggle value="project"
-                [checked]="!includeProject"
-                (change)="toggle($event)"
-                [matTooltip]="projectTT">
-            <span class="fa-stack">
-                <i class="fa fa-product-hunt fa-stack-1x"></i>
-                <i *ngIf="!includeProject" class="fa fa-ban fa-stack-2x"></i>
-            </span>
-        </mat-button-toggle>
-        <mat-button-toggle value="product"
-                [checked]="!includeProject"
-                (change)="toggle($event)"
-                [matTooltip]="productTT">
-            <span class="fa-stack">
-                <i class="fa fa-shopping-basket fa-stack-1x"></i>
-                <i *ngIf="!includeProduct" class="fa fa-ban fa-stack-2x"></i>
-            </span>
-        </mat-button-toggle>
-    </mat-button-toggle-group>
-    `
+    <mat-radio-group [formControl]="control">
+        <mat-radio-button [value]="null">All</mat-radio-button>
+        <mat-radio-button [value]="'project'">Projects only</mat-radio-button>
+        <mat-radio-button [value]="'product'">Products only</mat-radio-button>
+    </mat-radio-group>
+    `,
+    styles:[`
+        mat-radio-button {
+            padding-right: 10px;
+        }
+    `]
 })
 export class SctypeSelect implements SearchControl {
     control:FormControl;
-    includeProject;
-    projectTT;
-    includeProduct;
-    productTT;
 
     constructor(private search:SearchService) {
         let initial = search.initial,
             scType = initial ? initial.scType : null;
         this.control = new FormControl(scType);
-        this.init(scType);
         search.register(this);
-    }
-
-    private init(scType) {
-        switch(scType) {
-            case 'project':
-                this.includeProject = true;
-                this.includeProduct = false;
-                break;
-            case 'product':
-                this.includeProject = false;
-                this.includeProduct = true;
-                break;
-            default:
-                this.includeProject = true;
-                this.includeProduct = true;
-                break;
-        }
-        this.projectTT = `${this.includeProject ? 'Exclude' : 'Include'} projects`;
-        this.productTT = `${this.includeProduct ? 'Exclude' : 'Include'} products`;
     }
 
     reset() {
         this.control.setValue(null,{emitEvent:false});
-        this.init(null);
-    }
-
-    toggle(change:MatButtonToggleChange) {
-        if(change.value === 'project') {
-            if ( !(this.includeProject = !this.includeProject) && !this.includeProduct) {
-                this.includeProduct = true;
-            }
-        } else {
-            if( !(this.includeProduct = !this.includeProduct) && !this.includeProject) {
-                this.includeProject = true;
-            }
-        }
-        this.projectTT = `${this.includeProject ? 'Exclude' : 'Include'} projects`;
-        this.productTT = `${this.includeProduct ? 'Exclude' : 'Include'} products`;
-        this.control.setValue((this.includeProject && this.includeProduct) ? null : this.includeProject ? 'project' : 'product');
     }
 }
