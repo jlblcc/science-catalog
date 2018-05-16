c0f555619c1555cacc0180155701522ad7229bbf
Paul Adams <adamspe@gmail.com>
2018-05-16 15:59:04 -0500

    shorten date format in item-table.

--- app_src/src/app/search/item-date.component.ts
+++ app_src/src/app/search/item-date.component.ts
@@ -4,9 +4,10 @@ import { ItemIfc } from '../../../../src/db/models';
 @Component({
     selector: 'item-date',
     template: `
-    <span *ngIf="item.simplified.dates && item.simplified.dates.sort">{{item.simplified.dates.sort | date:'MM/dd/yyyy'}}</span>
+    <span *ngIf="item.simplified.dates && item.simplified.dates.sort">{{item.simplified.dates.sort | date:format}}</span>
     `
 })
 export class ItemDate {
     @Input() item:ItemIfc;
+    @Input() format:string = 'MM/dd/yyyy';
 }
